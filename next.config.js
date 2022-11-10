99e3a7996bd912eeeb0ba91a9e7ce024758f1f1f
Florante Clavano Jr <fgclavano@gmail.com>
2022-11-11 00:57:37 +0800

    disable typecsript rules

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
     loader: 'default',
     domains: ['cdn.sanity.io'],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
